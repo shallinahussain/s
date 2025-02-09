95345d175df082b37e86d485f44c6557c5235b6c
shallinahussain <shallinahussain@gmail.com>
2025-02-10 00:14:58 +0500

    portfolio

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <HeroSection/>
+    
+    <WhyChooseUs />
+    
+    <UpcomingWebinars/>
+    <Instructors />
+    <Footer />
+    </main>
+  );
+}
